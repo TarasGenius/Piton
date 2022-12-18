b3c4a2f7bf15c0de4c2642d301f280fd8b15e15d
TarasGenius <87486653+TarasGenius@users.noreply.github.com>
2022-12-18 10:24:16 +0200

    Add files via upload

--- task_13.py
+++ task_13.py
@@ -1,13 +1,13 @@
 
-
 class First:
     def __init__(self, a):
         self.a = a
         print(self.a)
+
     def method(self, *args):
         self.keys = set(map(tuple, args))
-        print(self.keys)
-        print(type(self.keys), len(self.keys))
+        return self.keys
+
 
 class Second(First):
     def __init__(self, b, c, a):
@@ -16,20 +16,15 @@ class Second(First):
         self.c = c
         print(self.b, self.c)
 
-
     def method(self, **kwargs):
-        super().method(set(map(str, kwargs.keys())))
         self.values = set(map(str, kwargs.values()))
-        print(self.values)
-        print(type(self.values))
-
-
+        return super().method(set(map(str, kwargs.keys()))), self.values
 
 
 obj = Second(1, 2, 3)
-objj= obj.method(name='Tom', age='12')
-
-
+a, b = obj.method(name='Tom', age='12', less='13')
+print(a)
+print(b)
 
 
 
